5017e95d20c930ddbb61b20fa0f368932ec66df8
TaherAmlaki <taher.amlaki@gmail.com>
2020-09-09 15:48:16 +0200

    Changes to styling

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from collections import Counter
 from .models import Article, Tag
 
 
+
 class ArticleListView(ListView):
     model = Article
     template_name = "blog/blog.html"
@@ -42,6 +43,9 @@ class TagArticleListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         tag = get_object_or_404(Tag, slug=self.kwargs.get("slug"))
-        context['TagName'] = tag.name
+        context['ActiveTags'] = [tag.name]
+
+        all_tags = list(Tag.objects.filter(article_tags__in=Article.objects.all()))
+        context['AllTags'] = dict(Counter(all_tags).most_common())
         return context
 
